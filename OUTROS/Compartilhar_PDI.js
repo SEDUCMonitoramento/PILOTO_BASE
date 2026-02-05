92f7b94492b658307a0163eef17c650e56d100db
SEDUC Monitoramento <monitoramento@seducbertioga.com.br>
2026-02-05 10:55:10 -0300

    Update: 2026-02-05 10:55:10

--- OUTROS/Compartilhar_PDI.js
+++ OUTROS/Compartilhar_PDI.js
@@ -16,7 +16,7 @@ function CompartilharPDI() {
       if (escola.getSheetByName(turma[0])) {
         for (let i = 7; i <= 70; i++) {
           escola.getSheetByName(turma[0]).getRange("AY" + i).setFormula('=IFERROR("https://docs.google.com/spreadsheets/d/"&PROCV($E' + i + '; AEEv24!$C$4:$F;3;0);" ")')
-          //escola.getSheetByName(turma[0]).getRange("AY"+i).setFormula('=SEERRO("https://docs.google.com/spreadsheets/d/"&PROCV($E' + i + '; AEEv25!$C$4:$F;3;0);" ")')
+          // escola.getSheetByName(turma[0]).getRange("AY"+i).setFormula('=SEERRO("https://docs.google.com/spreadsheets/d/"&PROCV($E' + i + '; AEEv25!$C$4:$F;3;0);" ")')
         }
       } else {
         console.log(`Turma ${turma[0]} não existe`)
